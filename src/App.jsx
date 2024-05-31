88b27a3ef8edb4d1505650ef271a28a9d8b394c5
Amrit <amritsinghal2211@gmail.com>
2024-05-31 22:58:06 +0530

    till heroImage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import NavBar from "./components/NavBar"
+import Home from "./components/Home"
 function App() {
 
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <div>
+     <NavBar/>
+     <Home/>
+    </div>
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import  HeroImage from '../assets/heroImage.jpg'
+const Home = () => {
+  return (
+    <div>
+        <img className = "h-[476px] w-[524px]"src={HeroImage} alt="" />
+    </div>
+  )
+}
+
+export default Home
